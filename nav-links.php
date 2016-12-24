6e8f77cd7c2ee63480241486865c7680348d1312
jsruesch <jake@gofarwell.com>
2016-12-23 21:36:42 -0600

    Change nav links to work on macbook

--- nav-links.php
+++ nav-links.php
@@ -1,3 +1,13 @@
+<li class="cstbtn"><a href="/gear/explore">Explore</a></li>
+<li class="cstbtn"><a href="/gear/projects">Projects</a></li>
+<li class="cstbtn"><a href="/gear/read">Read</a></li>
+<li class="cstbtn"><a href="/gear/jobs">Jobs</a></li>
+<li class="cstbtn"><a href="/gear/contact">Contact</a></li>
+<li class="cstbtn"><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+<li class="cstbtn"><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+
+
+<!--
 <li class="cstbtn"><a href="<?php echo get_template_directory_uri(); ?>/explore">Explore</a></li>
 <li class="cstbtn"><a href="<?php echo get_template_directory_uri(); ?>/projects">Projects</a></li>
 <li class="cstbtn"><a href="<?php echo get_template_directory_uri(); ?>/read">Read</a></li>
@@ -5,3 +15,4 @@
 <li class="cstbtn"><a href="<?php echo get_template_directory_uri(); ?>/contact">Contact</a></li>
 <li class="cstbtn"><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
 <li class="cstbtn"><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+-->

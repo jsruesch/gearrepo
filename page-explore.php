b349a564ca57456ef21a542bea11c3af1d05211f
jsruesch <jsruesch@gmail.com>
2017-01-04 08:37:58 -0600

    Add Keiths bio

--- page-explore.php
+++ page-explore.php
@@ -101,14 +101,14 @@
             </div><!-- END: John -->
           <!-- Keith -->
             <div class="col-sm-6 col-md-3 col-lg-2 pad5">
-              <a href="">
+              <a href="<?php echo get_template_directory_uri(); ?>/profile-keith">
               <div class="row portrait-container">
                 <div class="row ptext">
                   <div class="thickdiv mar10-bot"></div>
                   <h4 class="text-green">Keith<br />Decker</h4>
                   <p class="text-white small bold text-uppercase">Software Engineer</p>
                 </div>
-                <div class="portrait-btn none"></div>
+                <div class="portrait-btn keith"></div>
               </div>
               </a>
             </div><!-- END: Keith -->
---
+++ page-profile-keith.php
@@ -0,0 +1,89 @@
+<?php
+/*
+ Template Name: Profile Keith
+*/
+?>
+
+<?php get_header(); ?>
+<?php include ('nav.php'); ?>
+
+<!--
+  BEGIN: Main Content
+-->
+  <div class="container-fluid mar30-top mar20-bot pad30-left pad30-right">
+    <div class="container">
+    <!--
+      BEGIN: UNIFIED VISION
+      -->
+      <div class="row mar30-bot">
+        <div class="col-sm-3 pad10-right pad10-left">
+          <!-- Profile Image -->
+          <div class="profile-image-container bg-ddblue">
+            <div class="keith"></div>
+            <div class="corner-tr"></div>
+            <div class="corner-bl"></div>
+            <div class="profile-name">
+              <h3 class="text-green">Keith<br />Decker</h3>
+              <div class="thickdiv mar10-bot mar10-top"></div>
+              <h6 class="text-white small bold text-uppercase">Software Engineer</h6>
+            </div>
+          </div><!-- END: Profile -->
+          <div class="row bg-green pad30">
+            <h4 class="text-dblue"><i class="fa fa-star text-white" aria-hidden="true"></i> Skills</h4>
+            <div class="thickdiv mar10-bot mar10-top"></div>
+            <ul class="text-dblue disc">
+              <li>C++</li>
+              <li>C#</li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-sm-9 pad10-right pad10-left">
+          <div class="row bg-ddblue pad30">
+            <div class="row">
+              <p class="text-lblue">
+                I cut my teeth in Full Sail University's Game Development program and continued on to work at Creative Kingdoms after graduation. While there I had the opportunity to complete development on Cosmic Quest; an interactive educational exhibit spanning four of their facilities. After the project I had hoped to continue developing interactive educational media, eventually finding GEAR. I'm just as excited to work with GEAR as I was to start my first day at Full Sail.
+              </p>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm-4 bg-green pad30">
+              <h4 class="text-dblue"><i class="fa fa-trophy text-white" aria-hidden="true"></i> Projects</h4>
+              <div class="thickdiv mar10-bot mar10-top"></div>
+              <ul class="text-dblue disc">
+                <li>Kennedy Space Center's Cosmic Quest</li>
+              </ul>
+            </div>
+            <div class="col-sm-8 bg-lblue">
+              <div class="col-sm-6 pad30">
+                <h4 class="text-dblue"><i class="fa fa-gamepad text-white" aria-hidden="true"></i> Playing Now</h4>
+                <div class="thickdiv mar10-bot mar10-top"></div>
+                <ul class="text-dblue disc">
+                  <li>Warframe</li>
+                  <li>20XX</li>
+                  <li>Don't Starve Together</li>
+                </ul>
+              </div>
+              <div class="col-sm-6 pad30">
+                <h4 class="text-dblue"><i class="fa fa-heart text-white" aria-hidden="true"></i> Favorites</h4>
+                <div class="thickdiv mar10-bot mar10-top"></div>
+                <ul class="text-dblue disc">
+                  <li>Warframe</li>
+                  <li>Halo 2</li>
+                  <li>Fallout IV</li>
+                  <li>Kirby Super Star</li>
+                  <li>Diablo 2</li>
+                  <li>Mortal Kombat 3</li>
+                  <li>Brutal Doom</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    <!-- END: UNIFIED VISION -->
+
+    </div><!-- /.container -->
+  </div><!-- /.container-fluid -->
+<!-- END: Main Content -->
+
+<?php get_footer(); ?>

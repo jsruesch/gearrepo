9b63cd462bc2b3661b4380610aa25570c6c2bc13
jsruesch <jsruesch@gmail.com>
2017-01-05 10:58:58 -0600

    Add mike

---
+++ page-profile-mike.php
@@ -0,0 +1,98 @@
+<?php
+/*
+ Template Name: Profile Mike
+*/
+?>
+
+<?php get_header(); ?>
+<?php include ('nav.php'); ?>
+
+<!--
+  BEGIN: Main Content
+-->
+  <div class="container-fluid mar30-top mar20-bot pad30-left pad30-right">
+    <div class="container">
+    <!--
+      BEGIN: UNIFIED VISION
+      -->
+      <div class="row mar30-bot">
+        <div class="col-sm-3 pad10-right pad10-left">
+          <!-- Profile Image -->
+          <div class="profile-image-container bg-ddblue">
+            <div class="mike"></div>
+            <div class="corner-tr"></div>
+            <div class="corner-bl"></div>
+            <div class="profile-name">
+              <h3 class="text-green">Mike<br />Beall</h3>
+              <div class="thickdiv mar10-bot mar10-top"></div>
+              <h6 class="text-white small bold text-uppercase">Director</h6>
+            </div>
+          </div><!-- END: Profile -->
+          <div class="row bg-green pad30">
+            <h4 class="text-dblue"><i class="fa fa-star text-white" aria-hidden="true"></i> Skills</h4>
+            <div class="thickdiv mar10-bot mar10-top"></div>
+            <ul class="text-dblue disc">
+              <li>Project Management</li>
+              <li>Game Design</li>
+              <li>Unity3D</li>
+              <li>Maya</li>
+              <li>Adobe Creative Suite</li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-sm-9 pad10-right pad10-left">
+          <div class="row bg-ddblue pad30">
+            <div class="row">
+              <p class="text-lblue">
+                I am a lifelong game enthusiast with a professional history as a Game Designer, Project Leader, and Artist working with the Learning Games Network and Games Learning Society at the University of Wisconsin-Madison. I designed my first educational game in efforts to find a fun and engaging way to help my then 8 year old daughter study her vocabulary lists. In 2010 I was recruited to GLS by Professor Kurt Squire to create artwork for Virulent. I went on to lead design and development for several GLS/LGN projects including Progenitor X, Tenacity, Crystals of Kaydor, Oztoc, and At Play in the Cosmos. In addition to direct involvement with design and development of many GLS/LGN games, I also worked with local schools and community centers where I engaged with University of Wisconsin-Madison faculty and students to conduct playtests, interviews, and focus group tests.
+              </p>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm-4 bg-green pad30">
+              <h4 class="text-dblue"><i class="fa fa-trophy text-white" aria-hidden="true"></i> Projects</h4>
+              <div class="thickdiv mar10-bot mar10-top"></div>
+              <ul class="text-dblue disc">
+                <li>Virulent</li>
+                <li>Progenitor X</li>
+                <li>Tenacity</li>
+                <li>Crystals of Kaydor</li>
+                <li>Oztoc</li>
+                <li>Zoodles</li>
+                <li>Econauts</li>
+                <li>Xenos</li>
+                <li>At Play in the Cosmos</li>
+              </ul>
+            </div>
+            <div class="col-sm-8 bg-lblue">
+              <div class="col-sm-6 pad30">
+                <h4 class="text-dblue"><i class="fa fa-gamepad text-white" aria-hidden="true"></i> Playing Now</h4>
+                <div class="thickdiv mar10-bot mar10-top"></div>
+                <ul class="text-dblue disc">
+                  <li>Diablo 3</li>
+                  <li>Team Fortress 2</li>
+                  <li>Titan Quest</li>
+                </ul>
+              </div>
+              <div class="col-sm-6 pad30">
+                <h4 class="text-dblue"><i class="fa fa-heart text-white" aria-hidden="true"></i> Favorites</h4>
+                <div class="thickdiv mar10-bot mar10-top"></div>
+                <ul class="text-dblue disc">
+                  <li>Dig Dug</li>
+                  <li>Telengard</li>
+                  <li>Diablo 2</li>
+                  <li>Team Fortress 2</li>
+                  <li>Minecraft</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    <!-- END: UNIFIED VISION -->
+
+    </div><!-- /.container -->
+  </div><!-- /.container-fluid -->
+<!-- END: Main Content -->
+
+<?php get_footer(); ?>

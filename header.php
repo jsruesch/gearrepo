8441697d495e0267d5c369bdcb064deeb5301c8e
jsruesch <jsruesch@gmail.com>
2017-01-05 10:09:58 -0600

    Add ReCaptcha code to head

--- header.php
+++ header.php
@@ -46,6 +46,8 @@ new WOW().init();
 </script>
 <!-- Fontawesome -->
 <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+<!-- ReCaptcha -->
+<script src='https://www.google.com/recaptcha/api.js'></script>
 </head>
 
 <body <?php body_class(); ?>>

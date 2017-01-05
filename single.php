7e84dc26ddb79527240b10ac98594355dad9d72d
jsruesch <jsruesch@gmail.com>
2017-01-05 10:03:30 -0600

    Change link normal state color

--- single.php
+++ single.php
@@ -25,11 +25,11 @@
         <div class="row pad30 bg-ddblue">
           <h2 class="text-green"><?php the_title(); ?></h2>
           <div class="thickdiv mar10-bot mar10-top"></div>
-          <div class="text-lblue">By <?php the_author_posts_link(); ?> on <?php the_time('F j, Y') ?></div>		
+          <div class="text-lblue">By <?php the_author_posts_link(); ?> on <?php the_time('F j, Y') ?></div>
 
         </div>
         <div class="row pad20 bg-white">
-          <p  class="text-lblue">
+          <p  class="text-lblue bcopy">
             <?php the_content(); ?>
           </p>
         </div>

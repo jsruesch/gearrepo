ffff66c9db142a6ddc24016c0b0a7cd6b6be84d9
jsruesch <jsruesch@gmail.com>
2017-01-05 11:21:21 -0600

    Hide gearbox

--- page-blog.php
+++ page-blog.php
@@ -49,7 +49,7 @@
         <!--
           BEGIN: News & Updates
         -->
-          <div class="col-sm-4 pad20-top pad20-bot pad10-left pad10-right">
+          <div class="col-sm-6 pad20-top pad20-bot pad10-left pad10-right">
             <h3 class="text-green"><span class="text-white">News &</span> Updates</h3>
             <div class="row">
               <?php query_posts('category_name=news-and-updates&posts_per_page=1');?>
@@ -85,16 +85,16 @@
         <!--
           BEGIN: Gearbox
         -->
-          <div class="col-sm-4 pad20-top pad20-bot pad10-left pad10-right">
+          <!-- <div class="col-sm-4 pad20-top pad20-bot pad10-left pad10-right">
             <h3 class="text-green"><span class="text-white">GEAR</span>box</h3>
             <div class="row">
               <?php query_posts('category_name=gearbox&posts_per_page=1');?>
           		<?php if (have_posts()) : ?>
-          				<?php $post = $posts[0]; // Hack. Set $post so that the_date() works. ?>
-          				<?php /* If this is a category archive */ if (is_category()) { ?>
+          				<?php $post = $posts[0]; ?>
+          				<?php  if (is_category()) { ?>
           				<?php } ?>
           				<?php while (have_posts()) : the_post(); ?>
-                    <!-- The Content -->
+
             				<div <?php post_class() ?>>
                       <div class="row post-img-container mar10-top">
                         <a href="<?php the_permalink() ?>">
@@ -110,18 +110,18 @@
                         <p><span  class="text-lblue"><?php the_content( $more_link_text , $strip_teaser ); ?>
                         </span></p>
                       </div>
-                    </div><!-- /The Content -->
+                    </div>
                   <?php endwhile; ?>
                   <?php else : ?>
                     <p class="text-lblue">There are currently no items to display</p>
               <?php endif; ?>
             </div>
-          </div>
+          </div> -->
         <!-- END: Gearbox -->
         <!--
           BEGIN: Inside Gear
         -->
-          <div class="col-sm-4 pad20-top pad20-bot pad10-left pad10-right">
+          <div class="col-sm-6 pad20-top pad20-bot pad10-left pad10-right">
             <h3 class="text-green"><span class="text-white">Inside </span>GEAR</h3>
             <div class="row">
               <?php query_posts('category_name=inside-gear&posts_per_page=1');?>

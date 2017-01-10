60d175af7a974dbf0ded1eb8cf9591d55fef956e
jsruesch <jsruesch@gmail.com>
2017-01-10 10:33:08 -0600

    Hide youtube link

--- nav-links.php
+++ nav-links.php
@@ -6,7 +6,7 @@
 <div class="row footer-sm-buttons">
   <li class="cstbtn"><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
   <li class="cstbtn"><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-  <li class="cstbtn"><a href="#"><i class="fa fa-youtube" aria-hidden="true"></i></a></li>
+  <!-- <li class="cstbtn"><a href="#"><i class="fa fa-youtube" aria-hidden="true"></i></a></li> -->
   <li class="cstbtn"><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
 </div>
 

9b37b6a849182150e9246a9645b13fe391b1cdfd
jsruesch <jsruesch@gmail.com>
2017-01-13 13:54:43 -0600

    Truncate news to be just links

--- page-blog.php
+++ page-blog.php
@@ -52,7 +52,7 @@
           <div class="col-sm-6 pad20-top pad20-bot pad10-left pad10-right">
             <h3 class="text-green"><span class="text-white">News &</span> Updates</h3>
             <div class="row">
-              <?php query_posts('category_name=news-and-updates&posts_per_page=1');?>
+              <?php query_posts('category_name=news-and-updates&posts_per_page=15');?>
           		<?php if (have_posts()) : ?>
           				<?php $post = $posts[0]; // Hack. Set $post so that the_date() works. ?>
           				<?php /* If this is a category archive */ if (is_category()) { ?>
@@ -60,19 +60,8 @@
           				<?php while (have_posts()) : the_post(); ?>
                     <!-- The Content -->
             				<div <?php post_class() ?>>
-                      <div class="row post-img-container mar10-top">
-                        <a href="<?php the_permalink() ?>">
-                        <?php
-                          if ( has_post_thumbnail() ) {
-                          	the_post_thumbnail();
-                          }?>
-                        </a>
-                      </div>
-                      <div class="row pad30 bg-ddblue">
-                        <h4><a href="<?php the_permalink() ?>"  class="text-green"><?php the_title(); ?></a></h4>
-                        <div class="thickdiv mar10-bot mar10-top"></div>
-                        <p><span  class="text-lblue"><?php the_content( $more_link_text , $strip_teaser ); ?>
-                        </span></p>
+                      <div class="row mar10-top pad20-right pad20-left pad15-top  bg-ddblue">
+                        <h4 class="text-green text-hover-white"><?php the_content(); ?></h4>
                       </div>
                     </div><!-- /The Content -->
                   <?php endwhile; ?>

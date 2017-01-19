1400eb675f5f1445bd46c1a10b9ddfd3c37caab2
jsruesch <jake@gofarwell.com>
2017-01-19 17:44:12 -0600

    Clean up home v2 html

--- page-home2.php
+++ page-home2.php
@@ -44,14 +44,17 @@
 <!--
   BEGIN: Main Content
 -->
+  <!-- Mission -->
   <div class="container-fluid mission-container wow fadeIn">
     <div class="container">
       <div class="row pad60-left pad60-right">
           <h2 class="text-center text-white mission-text wow fadeInUp" data-wow-delay=".45s">We design and develop games to help make complex content <span class="text-green">accessible</span> and <span class="text-green">fun</span>.</h2>
       </div>
     </div>
-  </div>
-
+  </div><!-- END: Mission -->
+<!--
+  BEGIN: Featured Post
+-->
   <div class="container-fluid">
     <div class="container bg-green">
       <div class="col-sm-8">
@@ -86,17 +89,20 @@
             <?php endif; ?>
           </div><!-- ./row -->
         </div>
-      </div>
-
+      </div><!-- END: Featured Post -->
+    <!--
+      BEGIN: Stay Connected
+    -->
       <div class="col-sm-4">
         <div class="row wow fadeInUp pad30" data-wow-delay=".75s">
           <h4 class="text-ddblue"><span class="text-white">Stay</span> Connected</h4>
           <div class="thickdiv mar15-bot mar10-top"></div>
-          <div class="">
+          <!-- Twitter -->
+          <div class="row">
             <a class="twitter-timeline" data-height="250" href="https://twitter.com/HigherEdGames">Tweets by HigherEdGames</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
           </div>
+          <!-- SM Buttons -->
           <div class="row wow fadeInUp mar20-top" data-wow-delay="1.5s">
-
             <div class="col-xs-3 bg-lgreen">
               <div class="profile-link-button">
                 <a class="btn-gen" href="" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
@@ -117,18 +123,18 @@
                 <a class="btn-gen" href="" target="_blank"><i class="fa fa-vimeo" aria-hidden="true"></i></a>
               </div>
             </div>
-          </div>
-
-        </div>
-      </div>
-
+          </div><!-- END: SM Buttons -->
+        </div><!-- /.row -->
+      </div><!-- END: Stay Connected -->
+    <!--
+      BEGIN: Featured Projects
+    -->
       <div class="col-sm-12 pad60">
         <div class="row">
           <h3 class="text-ddblue"><span class="text-white">Featured</span> Projects</h3>
           <div class="thickdiv mar15-bot mar10-top"></div>
         </div>
-
-
+        <!-- Project 1 -->
         <div class="col-sm-4 wow fadeInUp" data-wow-delay=".25s">
           <div class="row bg-ddblue hp-featured-proj">
             <a href="at-play-in-the-cosmos">
@@ -143,7 +149,8 @@
                 </div>
               </a>
           </div>
-        </div>
+        </div><!-- END: Project 2 -->
+        <!-- Project 2 -->
         <div class="col-sm-4 wow fadeInUp" data-wow-delay=".45s">
           <div class="row bg-ddblue hp-featured-proj">
             <a href="http://www.gearlearning.org/microsites/econauts/" target="_blank">
@@ -158,7 +165,8 @@
                 </div>
               </a>
           </div>
-        </div>
+        </div><!-- END: Project 2 -->
+        <!-- Project 3 -->
         <div class="col-sm-4 wow fadeInUp" data-wow-delay=".65s">
           <div class="row bg-ddblue hp-featured-proj">
             <a href="http://www.gearlearning.org/microsites/progenitor/" target="_blank">
@@ -173,39 +181,35 @@
                 </div>
               </a>
           </div>
-        </div>
-
-      </div>
+        </div><!-- END: Project 3 -->
+      </div><!-- /.col-sm-12 -->
     </div><!-- /.container -->
-  </div><!-- /.container-fluit -->
-
+  </div><!-- /.container-fluid -->
+<!--
+  BEGIN: Recent Release
+-->
   <div class="container-fluid">
     <div class="container bg-ddblue">
-    <div class="col-sm-8">
-      <!--
-        BEGIN: Recent Release
-      -->
-      <a href="/at-play-in-the-cosmos">
-        <div class="row wow fadeInUp" data-wow-delay=".25s">
-            <div class="recent-release-img-container">
-                <img src="<?php echo get_template_directory_uri(); ?>/img/projects/cosmos/featured-image-cosmos-01.png"/>
-                <div class="row pad30 bg-ddblue recent-release-title-container">
-                  <h3 class="text-white">Recent Release</h3>
-                  <h1 class="text-green">At Play in the Cosmos</h1>
-                  <div class="thickdiv mar10-bot mar10-top"></div>
-                  <p class="text-lblue">
-                    At Play in the Cosmos takes concepts from the textbook and transforms them in to interactive, intuitive tools that help students experience content in a way like never before.
-                  </p>
-                </div>
-            </div>
-        </div>
-      </a>
-      <!-- END: Recent Release -->
-    </div>
-
-  </div>
-</div><!-- /.container -->
+      <div class="col-sm-8">
+        <a href="/at-play-in-the-cosmos">
+          <div class="row wow fadeInUp" data-wow-delay=".25s">
+              <div class="recent-release-img-container">
+                  <img src="<?php echo get_template_directory_uri(); ?>/img/projects/cosmos/featured-image-cosmos-01.png"/>
+                  <div class="row pad30 bg-ddblue recent-release-title-container">
+                    <h3 class="text-white">Recent Release</h3>
+                    <h1 class="text-green">At Play in the Cosmos</h1>
+                    <div class="thickdiv mar10-bot mar10-top"></div>
+                    <p class="text-lblue">
+                      At Play in the Cosmos takes concepts from the textbook and transforms them in to interactive, intuitive tools that help students experience content in a way like never before.
+                    </p>
+                  </div>
+              </div>
+          </div>
+        </a>
+      </div><!-- /.col-sm-8 -->
+    </div><!-- /container -->
   </div><!-- /.container-fluid -->
+<!-- END: Recent Release -->
 <!-- END: Main Content -->
 
 <?php get_footer(); ?>

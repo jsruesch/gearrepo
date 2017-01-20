bc77b2c7b3a03b5fcf055e279050d5baf3536801
jsruesch <jsruesch@gmail.com>
2017-01-20 13:56:09 -0600

    Remove p

--- single.php
+++ single.php
@@ -28,7 +28,7 @@
           <div class="text-lblue">By <?php the_author_posts_link(); ?> on <?php the_time('F j, Y') ?></div>
 
         </div>
-        <div class="row pad20 bg-white post-entry">
+        <div class="row pad20 bg-white">
           <p  class="text-lblue bcopy">
             <?php the_content(); ?>
           </p>

4170f93bfd42883d6bd1b0f608ffe9ae67c32778
jsruesch <jake@gofarwell.com>
2017-01-19 21:36:31 -0600

    Restructure keiths bio code

--- page-profile-keith.php
+++ page-profile-keith.php
@@ -10,13 +10,13 @@
 <!--
   BEGIN: Main Content
 -->
-  <div class="container-fluid mar30-top mar20-bot">
-    <div class="container pad30-left pad30-right">
+  <div class="container-fluid">
+    <div class="container">
     <!--
       BEGIN: UNIFIED VISION
       -->
-      <div class="row mar30-bot">
-        <div class="col-sm-3 pad10-right pad10-left">
+      <div class="row">
+        <div class="col-sm-3">
           <!-- Profile Image -->
           <div class="profile-image-container bg-ddblue">
             <div class="keith"></div>
@@ -44,45 +44,43 @@
             </div>
           </div>
         </div>
-        <div class="col-sm-9 pad10-right pad10-left">
-          <div class="row bg-ddblue pad30">
+        <div class="col-sm-9">
+          <div class="row pad60">
             <div class="row">
               <p class="text-lblue">
                 I cut my teeth in Full Sail University's Game Development program and continued on to work at Creative Kingdoms after graduation. While there I had the opportunity to complete development on Cosmic Quest; an interactive educational exhibit spanning four of their facilities. After the project I had hoped to continue developing interactive educational media, eventually finding GEAR. I'm just as excited to work with GEAR as I was to start my first day at Full Sail.
               </p>
             </div>
           </div>
-          <div class="row">
-            <div class="col-sm-4 bg-green pad30">
-              <h4 class="text-dblue"><i class="fa fa-trophy text-white" aria-hidden="true"></i> Projects</h4>
+          <div class="row pad30 bg-ddblue">
+            <div class="col-sm-4 pad15">
+              <h4 class="text-green"><i class="fa fa-trophy text-white" aria-hidden="true"></i> Projects</h4>
               <div class="thickdiv mar10-bot mar10-top"></div>
-              <ul class="text-dblue disc">
+              <ul class="text-lblue disc">
                 <li>Kennedy Space Center's Cosmic Quest</li>
               </ul>
             </div>
-            <div class="col-sm-8 bg-lblue">
-              <div class="col-sm-6 pad30">
-                <h4 class="text-dblue"><i class="fa fa-gamepad text-white" aria-hidden="true"></i> Playing Now</h4>
-                <div class="thickdiv mar10-bot mar10-top"></div>
-                <ul class="text-dblue disc">
-                  <li>Warframe</li>
-                  <li>20XX</li>
-                  <li>Don't Starve Together</li>
-                </ul>
-              </div>
-              <div class="col-sm-6 pad30">
-                <h4 class="text-dblue"><i class="fa fa-heart text-white" aria-hidden="true"></i> Favorites</h4>
-                <div class="thickdiv mar10-bot mar10-top"></div>
-                <ul class="text-dblue disc">
-                  <li>Warframe</li>
-                  <li>Halo 2</li>
-                  <li>Fallout IV</li>
-                  <li>Kirby Super Star</li>
-                  <li>Diablo 2</li>
-                  <li>Mortal Kombat 3</li>
-                  <li>Brutal Doom</li>
-                </ul>
-              </div>
+            <div class="col-sm-4 pad15">
+              <h4 class="text-green"><i class="fa fa-gamepad text-white" aria-hidden="true"></i> Playing Now</h4>
+              <div class="thickdiv mar10-bot mar10-top"></div>
+              <ul class="text-lblue disc">
+                <li>Warframe</li>
+                <li>20XX</li>
+                <li>Don't Starve Together</li>
+              </ul>
+            </div>
+            <div class="col-sm-4 pad15">
+              <h4 class="text-green"><i class="fa fa-heart text-white" aria-hidden="true"></i> Favorites</h4>
+              <div class="thickdiv mar10-bot mar10-top"></div>
+              <ul class="text-lblue disc">
+                <li>Warframe</li>
+                <li>Halo 2</li>
+                <li>Fallout IV</li>
+                <li>Kirby Super Star</li>
+                <li>Diablo 2</li>
+                <li>Mortal Kombat 3</li>
+                <li>Brutal Doom</li>
+              </ul>
             </div>
           </div>
         </div>

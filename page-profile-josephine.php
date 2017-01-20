30be3e199ef549532fe4da6d3209fadf87fc0a94
jsruesch <jake@gofarwell.com>
2017-01-19 21:45:35 -0600

    Restructure josephines bio code

--- page-profile-josephine.php
+++ page-profile-josephine.php
@@ -10,13 +10,13 @@
 <!--
   BEGIN: Main Content
 -->
-  <div class="container-fluid mar30-top mar20-bot">
-    <div class="container pad30-left pad30-right">
+  <div class="container-fluid">
+    <div class="container">
     <!--
       BEGIN: UNIFIED VISION
       -->
-      <div class="row mar30-bot">
-        <div class="col-sm-3 pad10-right pad10-left">
+      <div class="row">
+        <div class="col-sm-3">
           <!-- Profile Image -->
           <div class="profile-image-container bg-ddblue">
             <div class="josephine"></div>
@@ -37,7 +37,7 @@
               <li>zBrush</li>
               <li>Photoshop</li>
               <li>Substance Painter</li>
-              <li>Unity</li>
+              <li>Unity3D</li>
             </ul>
           </div>
           <div class="row bg-ddblue">
@@ -58,42 +58,40 @@
             </div>
           </div>
         </div>
-        <div class="col-sm-9 pad10-right pad10-left">
-          <div class="row bg-ddblue pad30">
+        <div class="col-sm-9">
+          <div class="row pad60">
             <div class="row">
               <p class="text-lblue">
                 Hello, I am a graduate of MATC in 2016 with an Associates of Art Degree in Animation and Concept Development. I began interning with the team at Learning Games Network in October of 2015. Since then I have been brought onto the team as it’s newest artist creating animations, 3d models and concept art.
               </p>
             </div>
           </div>
-          <div class="row">
-            <div class="col-sm-4 bg-green pad30">
-              <h4 class="text-dblue"><i class="fa fa-trophy text-white" aria-hidden="true"></i> Projects</h4>
+          <div class="row bg-ddblue pad30">
+            <div class="col-sm-4 pad15">
+              <h4 class="text-green"><i class="fa fa-trophy text-white" aria-hidden="true"></i> Projects</h4>
               <div class="thickdiv mar10-bot mar10-top"></div>
-              <ul class="text-dblue disc">
+              <ul class="text-lblue disc">
                 <li>At Play in the Cosmos</li>
               </ul>
             </div>
-            <div class="col-sm-8 bg-lblue">
-              <div class="col-sm-6 pad30">
-                <h4 class="text-dblue"><i class="fa fa-gamepad text-white" aria-hidden="true"></i> Playing Now</h4>
-                <div class="thickdiv mar10-bot mar10-top"></div>
-                <ul class="text-dblue disc">
-                  <li>Team Fortress 2</li>
-                  <li>Skyrim Special Edition</li>
-                  <li>Dark Souls III</li>
-                </ul>
-              </div>
-              <div class="col-sm-6 pad30">
-                <h4 class="text-dblue"><i class="fa fa-heart text-white" aria-hidden="true"></i> Favorites</h4>
-                <div class="thickdiv mar10-bot mar10-top"></div>
-                <ul class="text-dblue disc">
-                  <li>Elder Scrolls III: Morrowind</li>
-                  <li>Fallout New Vegas</li>
-                  <li>Outlast</li>
-                  <li>Silent Hill 2</li>
-                </ul>
-              </div>
+            <div class="col-sm-4 pad15">
+              <h4 class="text-green"><i class="fa fa-gamepad text-white" aria-hidden="true"></i> Playing Now</h4>
+              <div class="thickdiv mar10-bot mar10-top"></div>
+              <ul class="text-lblue disc">
+                <li>Team Fortress 2</li>
+                <li>Skyrim Special Edition</li>
+                <li>Dark Souls III</li>
+              </ul>
+            </div>
+            <div class="col-sm-4 pad15">
+              <h4 class="text-green"><i class="fa fa-heart text-white" aria-hidden="true"></i> Favorites</h4>
+              <div class="thickdiv mar10-bot mar10-top"></div>
+              <ul class="text-lblue disc">
+                <li>Elder Scrolls III: Morrowind</li>
+                <li>Fallout New Vegas</li>
+                <li>Outlast</li>
+                <li>Silent Hill 2</li>
+              </ul>
             </div>
           </div>
         </div>

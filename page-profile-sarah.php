a25ee5493b37919e8198c2520b2cd369c96cffee
jsruesch <jake@gofarwell.com>
2017-01-19 21:40:16 -0600

    Restructure sarahs bio code

--- page-profile-sarah.php
+++ page-profile-sarah.php
@@ -10,13 +10,13 @@
 <!--
   BEGIN: Main Content
 -->
-  <div class="container-fluid mar30-top mar20-bot">
-    <div class="container pad30-left pad30-right">
+  <div class="container-fluid">
+    <div class="container">
     <!--
       BEGIN: UNIFIED VISION
       -->
-      <div class="row mar30-bot">
-        <div class="col-sm-3 pad10-right pad10-left">
+      <div class="row">
+        <div class="col-sm-3">
           <!-- Profile Image -->
           <div class="profile-image-container bg-ddblue">
             <div class="sarah"></div>
@@ -47,19 +47,19 @@
             </div>
           </div>
         </div>
-        <div class="col-sm-9 pad10-right pad10-left">
-          <div class="row bg-ddblue pad30">
+        <div class="col-sm-9">
+          <div class="row pad60">
             <div class="row">
               <p class="text-lblue">
                 I obtained my Bachelor's in Art from the University of Wisconsin - Madison.  After working for three years as a project manager for a software company, I returned to school and graduated from the Applied Arts in Animation & Concept Development program at Madison College. I began my 3D artist career as an intern for Learning Games Network in early 2014, which progressed to a full time staff artist later that year.  Now an Associate 3D artist for GEAR, I continue to work on educational games, with a focus on medically accurate human and animal simulations designed for students to learn anatomy.
               </p>
             </div>
           </div>
-          <div class="row">
-            <div class="col-sm-4 bg-green pad30">
-              <h4 class="text-dblue"><i class="fa fa-trophy text-white" aria-hidden="true"></i> Projects</h4>
+          <div class="row bg-ddblue pad30">
+            <div class="col-sm-4 pad15">
+              <h4 class="text-green"><i class="fa fa-trophy text-white" aria-hidden="true"></i> Projects</h4>
               <div class="thickdiv mar10-bot mar10-top"></div>
-              <ul class="text-dblue disc">
+              <ul class="text-lblue disc">
                 <li>Ross Medical and Veterinary Games and Simulations </li>
                 <li>Anatomy Pro-Am </li>
                 <li>At Play in the Cosmos </li>
@@ -67,24 +67,22 @@
                 <li>Anne & Emmet</li>
               </ul>
             </div>
-            <div class="col-sm-8 bg-lblue">
-              <div class="col-sm-6 pad30">
-                <h4 class="text-dblue"><i class="fa fa-gamepad text-white" aria-hidden="true"></i> Playing Now</h4>
-                <div class="thickdiv mar10-bot mar10-top"></div>
-                <ul class="text-dblue disc">
-                  <li>Team Fortress 2</li>
-                </ul>
-              </div>
-              <div class="col-sm-6 pad30">
-                <h4 class="text-dblue"><i class="fa fa-heart text-white" aria-hidden="true"></i> Favorites</h4>
-                <div class="thickdiv mar10-bot mar10-top"></div>
-                <ul class="text-dblue disc">
-                  <li>Team Fortress 2</li>
-                  <li>Last of Us</li>
-                  <li>Bloodborne</li>
-                  <li>Guild Wars 2</li>
-                </ul>
-              </div>
+            <div class="col-sm-4 pad15">
+              <h4 class="text-green"><i class="fa fa-gamepad text-white" aria-hidden="true"></i> Playing Now</h4>
+              <div class="thickdiv mar10-bot mar10-top"></div>
+              <ul class="text-lblue disc">
+                <li>Team Fortress 2</li>
+              </ul>
+            </div>
+            <div class="col-sm-4 pad15">
+              <h4 class="text-green"><i class="fa fa-heart text-white" aria-hidden="true"></i> Favorites</h4>
+              <div class="thickdiv mar10-bot mar10-top"></div>
+              <ul class="text-lblue disc">
+                <li>Team Fortress 2</li>
+                <li>Last of Us</li>
+                <li>Bloodborne</li>
+                <li>Guild Wars 2</li>
+              </ul>
             </div>
           </div>
         </div>

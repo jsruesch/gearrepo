5afae0b1a0c2f5da40ddf1e12e2744095c88dc51
jsruesch <jsruesch@gmail.com>
2017-01-24 10:52:26 -0600

    Add CORI section to cosmos page

--- page-project-cosmos.php
+++ page-project-cosmos.php
@@ -39,70 +39,85 @@
         </div>
       </div>
 
-      <div class="row pad60 bg-ddblue">
-        <div class="row">
-          <div class="col-sm-2 pad20">
+    <!--
+      BEGIN: CORI
+    -->
+      <div class="row">
+        <div class="row pad60 bg-c-teal">
+          <div class="col-sm-2 pad20 bg-ddblue">
             <img class="center-block" src="<?php echo get_template_directory_uri(); ?>/img/projects/cosmos/icon-cori.svg" />
           </div>
-          <div class="col-sm-10">
-            <h2 class="text-c-teal">Introducing CORI</h2>
+          <div class="col-sm-10 pad20">
+            <h2 class="text-ddblue">Introducing CORI</h2>
             <div class="thickdiv mar10-bot mar10-top"></div>
-            <p class=" text-lblue">
-              CORI, the <strong><span class="text-c-teal">C</span><span class="text-white">osmic <span class="text-c-teal">O</span><span class="text-white">perations</span> <span class="text-c-teal">R</span><span class="text-white">esearch</span> <span class="text-c-teal">I</span><span class="text-white">nterface</span></strong>, is your students' control center within the game. At any time, students can tap this button to access:
+            <p class=" text-ddblue">
+              CORI, the <strong><span class="text-white"><u>C</u>osmic <u>O</u>perations <u>R</u>esearch <u>I</u>nterface</strong>, is your students' control center within the game. At any time, students can tap this button to access:
             </p>
           </div>
         </div>
-
-
-        <div class="row">
-          <div class="col-sm-2 pad20">
-            <img class="center-block" src="<?php echo get_template_directory_uri(); ?>/img/projects/cosmos/icon-starchart.svg" />
-          </div>
-          <div class="col-sm-10">
-            <h3 class="text-c-teal">Star Chart</h3>
-            <p class="text-lblue">
-              The Interactive Star Chart, for travel throughout the universe and access to onboard scientific analysis tools, such as the Spectrum Analyzer and Small Angle Tool.
-            </p>
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="col-sm-2 pad20">
-            <img class="center-block" src="<?php echo get_template_directory_uri(); ?>/img/projects/cosmos/icon-missions.svg" />
-          </div>
-          <div class="col-sm-10">
-            <h3 class="text-c-green">Missions</h3>
-            <p class="text-lblue">
-
-            </p>
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="col-sm-2 pad20">
-            <img class="center-block" src="<?php echo get_template_directory_uri(); ?>/img/projects/cosmos/icon-data.svg" />
-          </div>
-          <div class="col-sm-10">
-            <h3 class="text-c-pink">Data</h3>
-            <p class="text-lblue">
-
-            </p>
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="col-sm-2 pad20">
-            <img class="center-block" src="<?php echo get_template_directory_uri(); ?>/img/projects/cosmos/icon-engineering.svg" />
-          </div>
-          <div class="col-sm-10">
-            <h3 class="text-c-yellow">Engineering</h3>
-            <p class="text-lblue">
-
-            </p>
+        <div class="row pad60 bg-ddblue">
+          <div class="row">
+            <!-- Star Chart -->
+            <div class="col-sm-6 pad10">
+              <div class="row">
+                <div class="col-sm-4 pad20">
+                  <img class="center-block cosmos-svg" src="<?php echo get_template_directory_uri(); ?>/img/projects/cosmos/icon-starchart.svg" />
+                </div>
+                <div class="col-sm-8">
+                  <h3 class="text-c-teal">Star Chart</h3>
+                  <p class="text-lblue">
+                    The Interactive Star Chart, for travel throughout the universe and access to onboard scientific analysis tools, such as the Spectrum Analyzer and Small Angle Tool.
+                  </p>
+                </div>
+              </div>
+            </div><!-- END: Star Chart -->
+            <!-- Missions -->
+            <div class="col-sm-6 pad10">
+              <div class="row">
+                <div class="col-sm-4 pad20">
+                  <img class="center-block cosmos-svg" src="<?php echo get_template_directory_uri(); ?>/img/projects/cosmos/icon-missions.svg" />
+                </div>
+                <div class="col-sm-8">
+                  <h3 class="text-c-green">Missions</h3>
+                  <p class="text-lblue">
+                    The Missions Button, for acces to the missions list, or to switch to Sandbox Mode
+                  </p>
+                </div>
+              </div>
+            </div><!-- END: Missions -->
+          </div><!-- /.row -->
+          <div class="row">
+            <div class="col-sm-6 pad10">
+              <div class="row">
+                <div class="col-sm-4 pad20">
+                  <img class="center-block cosmos-svg" src="<?php echo get_template_directory_uri(); ?>/img/projects/cosmos/icon-data.svg" />
+                </div>
+                <div class="col-sm-8">
+                  <h3 class="text-c-pink">Data</h3>
+                  <p class="text-lblue">
+                    The Data Almanac, for access to the Interactive Simulations and Glassary. Students can also track their Achievements here.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-6 pad10">
+              <div class="row">
+                <div class="col-sm-4 pad20">
+                  <img class="center-block cosmos-svg" src="<?php echo get_template_directory_uri(); ?>/img/projects/cosmos/icon-engineering.svg" />
+                </div>
+                <div class="col-sm-8">
+                  <h3 class="text-c-yellow">Engineering</h3>
+                  <p class="text-lblue">
+                    The Engineering Button, where students can access the Crafting System used to repair the ship
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-
+    <!-- END: CORI -->
+    
       <!-- Tools -->
       <!-- <div class="row mar20-bot">
         <div class="col-sm-6">

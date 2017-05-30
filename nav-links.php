46844769445906905c3ca144b800d55f652b4e19
jsruesch <jsruesch@gmail.com>
2017-05-30 10:18:03 -0500

    Make jobs visible and hide various positions

--- nav-links.php
+++ nav-links.php
@@ -1,7 +1,7 @@
 <li class="cstbtn"><a href="/gear/explore">Explore</a></li>
 <li class="cstbtn"><a href="/gear/projects">Projects</a></li>
 <li class="cstbtn"><a href="/gear/read">Read</a></li>
-<!-- <li class="cstbtn"><a href="/gear/jobs">Jobs</a></li> -->
+<li class="cstbtn"><a href="/gear/jobs">Jobs</a></li>
 <li class="cstbtn"><a href="/gear/contact">Contact</a></li>
 <div class="row footer-sm-buttons"><!-- footer-sm-buttons used to hide from main nav -->
   <li class="cstbtn"><a href="https://twitter.com/GearLearning" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
--- page-jobs.php
+++ page-jobs.php
@@ -15,17 +15,16 @@
       <div class="row pad20-top pad10-bot">
         <h1 class="text-green"><span class="text-white">GEAR</span> Jobs</h1>
       </div>
-
+<!--
       <div class="row pad10-top pad10-bot">
         <h3 class="text-green pad10-bot"><span class="text-white">Game Tester</span> Positions</h3>
         <div class="row bg-ddblue pad30-left pad30-right pad20-top pad20-bot">
           <?php query_posts('category_name=game-tester-position');?>
           <?php if (have_posts()) : ?>
-              <?php $post = $posts[0]; // Hack. Set $post so that the_date() works. ?>
-              <?php /* If this is a category archive */ if (is_category()) { ?>
+              <?php $post = $posts[0]; ?>
+              <?php if (is_category()) { ?>
               <?php } ?>
               <?php while (have_posts()) : the_post(); ?>
-                <!-- The Content -->
                 <div <?php post_class() ?>>
                   <div class="row">
                     <a href="<?php the_permalink() ?>">
@@ -38,7 +37,7 @@
                   <div class="row pad5-top pad5-bot">
                     <h4 class="text-white"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h4>
                   </div>
-                </div><!-- /The Content -->
+                </div>
               <?php endwhile; ?>
               <?php else : ?>
                 <p class="text-lblue">We curently have no <span class="text-green">Game Tester Positions</span> available. Please check back later!</p>
@@ -51,11 +50,10 @@
         <div class="row bg-ddblue pad30">
           <?php query_posts('category_name=engineer-position');?>
           <?php if (have_posts()) : ?>
-              <?php $post = $posts[0]; // Hack. Set $post so that the_date() works. ?>
-              <?php /* If this is a category archive */ if (is_category()) { ?>
+              <?php $post = $posts[0]; ?>
+              <?php if (is_category()) { ?>
               <?php } ?>
               <?php while (have_posts()) : the_post(); ?>
-                <!-- The Content -->
                 <div <?php post_class() ?>>
                   <div class="row">
                     <a href="<?php the_permalink() ?>">
@@ -68,7 +66,7 @@
                   <div class="row pad5-top pad5-bot">
                     <h4 class="text-white"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h4>
                   </div>
-                </div><!-- /The Content -->
+                </div>
               <?php endwhile; ?>
               <?php else : ?>
                 <p class="text-lblue">We curently have no <span class="text-green">Engineering Positions</span> available. Please check back later!</p>
@@ -81,11 +79,11 @@
         <div class="row bg-ddblue pad30">
           <?php query_posts('category_name=artist-position');?>
           <?php if (have_posts()) : ?>
-              <?php $post = $posts[0]; // Hack. Set $post so that the_date() works. ?>
-              <?php /* If this is a category archive */ if (is_category()) { ?>
+              <?php $post = $posts[0]; ?>
+              <?php if (is_category()) { ?>
               <?php } ?>
               <?php while (have_posts()) : the_post(); ?>
-                <!-- The Content -->
+
                 <div <?php post_class() ?>>
                   <div class="row">
                     <a href="<?php the_permalink() ?>">
@@ -98,16 +96,17 @@
                   <div class="row pad5-top pad5-bot">
                     <h4 class="text-white"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h4>
                   </div>
-                </div><!-- /The Content -->
+                </div>
               <?php endwhile; ?>
               <?php else : ?>
                 <p class="text-lblue">We curently have no <span class="text-green">Artist Positions</span> available. Please check back later!</p>
           <?php endif; ?>
         </div>
       </div>
+    -->
       <!-- Other -->
       <div class="row pad10-top pad10-bot">
-        <h3 class="text-green pad10-bot"><span class="text-white">Additional </span> Positions</h3>
+        <h3 class="text-green pad10-bot"><span class="text-white">Available </span> Positions</h3>
         <div class="row bg-ddblue pad30">
           <?php query_posts('category_name=additional-position');?>
           <?php if (have_posts()) : ?>

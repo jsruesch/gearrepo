1b80bc3883e56d4e32f1b643c9de0ad62ee9130b
jsruesch <jsruesch@gmail.com>
2019-01-03 09:41:25 -0600

    add gutenberg code to functions

--- functions.php
+++ functions.php
@@ -33,4 +33,34 @@
 	add_theme_support( 'post-thumbnails' );
 
 	add_theme_support( 'title-tag' );
+
+	/**
+* Add support for Gutenberg.
+*
+* @link https://wordpress.org/gutenberg/handbook/reference/theme-support/
+*/
+function mytheme_setup_theme_supported_features() {
+		
+	// Theme supports wide images, galleries and videos.
+	add_theme_support( 'align-wide' );
+	
+	// Make specific theme colors available in the editor.
+ add_theme_support( 'editor-color-palette',
+	  array(
+			'name' => 'dark blue',
+			'color' => '#1767ef',
+	  ),
+	  array(
+			'name' => 'light gray',
+			'color' => '#eee',
+	  ),
+	  array(
+			'name' => 'dark gray',
+			'color' => '#444',
+	  )
+ );
+}
+
+add_action( 'after_setup_theme', 'mytheme_setup_theme_supported_features' );
+
 ?>
